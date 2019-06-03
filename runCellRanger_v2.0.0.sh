61541ed852665dda4ca8a64bf4dfbadbe2ef51dc
Laura Richards <49448692+LauRich05@users.noreply.github.com>
2019-06-03 10:56:40 -0400

    Update runCellRanger_v2.0.0.sh

--- runCellRanger_v2.0.0.sh
+++ runCellRanger_v2.0.0.sh
@@ -13,4 +13,11 @@ cd /mnt/work1/users/pughlab/projects/BTSCs_scRNAseq/Manuscript_G607removed/data/
 CELLRANGER_ROOT='/mnt/work1/users/pughlab/bin/CellRanger/v2.0/cellranger-2.0.0/cellranger'
 TOOL='count'
 
-$CELLRANGER_ROOT $TOOL --id=$1 --sample=$2 --transcriptome=/mnt/work1/users/pughlab/references/refdata-cellranger-GRCh38-1.2.0 --fastqs=/mnt/work1/users/pughlab/projects/BTSCs_scRNAseq/Manuscript_G607removed/data/fastqs/$1 --expect-cells=3000 --jobmode=sge --mempercore=6 --maxjobs=40
+$CELLRANGER_ROOT $TOOL --id=$1 \
+--sample=$2 \
+--transcriptome=/mnt/work1/users/pughlab/references/refdata-cellranger-GRCh38-1.2.0 \
+--fastqs=/mnt/work1/users/pughlab/projects/BTSCs_scRNAseq/Manuscript_G607removed/data/fastqs/$1 \
+--expect-cells=3000 \
+--jobmode=sge \
+--mempercore=6 \
+--maxjobs=40
