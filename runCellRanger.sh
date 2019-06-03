ea3b59f58a734f84c69700bcc0d82512853fea69
Laura Richards <49448692+LauRich05@users.noreply.github.com>
2019-06-03 10:52:35 -0400

    Update runCellRanger.sh

--- runCellRanger.sh
+++ runCellRanger.sh
@@ -1,1 +1,15 @@
-list.files()
+#!/bin/bash
+#
+#$ -cwd
+
+
+#####$1 is the sample prefix
+#####$2 is the prefex on the fastq
+
+mkdir /mnt/work1/users/pughlab/projects/BTSCs_scRNAseq/Manuscript_G607removed/data/CellRanger_v2.1.0/$1
+cd /mnt/work1/users/pughlab/projects/BTSCs_scRNAseq/Manuscript_G607removed/data/CellRanger_v2.1.0/$1
+
+CELLRANGER_ROOT='/mnt/work1/users/pughlab/bin/CellRanger/v2.0/cellranger-2.0.0/cellranger'
+TOOL='count'
+
+$CELLRANGER_ROOT $TOOL --id=$1 --sample=$2 --transcriptome=/mnt/work1/users/pughlab/references/refdata-cellranger-GRCh38-1.2.0 --fastqs=/mnt/work1/users/pughlab/projects/BTSCs_scRNAseq/Manuscript_G607removed/data/fastqs/$1 --expect-cells=3000 --jobmode=sge --mempercore=6 --maxjobs=40
