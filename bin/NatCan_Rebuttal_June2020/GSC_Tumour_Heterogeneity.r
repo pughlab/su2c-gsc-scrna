c734a1d6998c3ddf7dc8cd8f4aaa33bb584356f3
Laura Richards <laura.michelle.richards@gmail.com>
2020-07-03 23:35:18 -0400

    start

---
+++ bin/NatCan_Rebuttal_June2020/GSC_Tumour_Heterogeneity.r
@@ -0,0 +1,17 @@
+##############################################################
+#        Compare hetereogeneity between GSCs + Tumours       #
+#                         L.Richards                         #
+#                         July 2020                          #
+##############################################################
+### REF: https://github.com/JinmiaoChenLab/Rphenograph
+### /cluster/projects/pughlab/projects/BTSCs_scRNAseq/Manuscript_G607removed/NatCan_Rebuttal/PCA_SampleScaling
+
+
+##############################################################
+### GENERAL OVERVIEW OF THIS SCRIPT
+### 1) Load GSC + tumour dataset
+### 2) Correlate cells within GSCs and within tumour samples
+### 3) Cluster tumours with pipeline (WIP)
+### 4) save data
+### 5) Plot violing plots of correlation distributions
+##############################################################
